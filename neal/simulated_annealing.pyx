ccb3311308e788c2e907122298d8e3d7617a5f45
kemalaziezrachmansyah <108319129+kemalaziezrachmansyah@users.noreply.github.com>
2022-07-04 14:40:55 +0900

    Update simulated_annealing.pyx

--- neal/simulated_annealing.pyx
+++ neal/simulated_annealing.pyx
@@ -129,6 +129,8 @@ def simulated_annealing(num_samples, h, coupler_starts, coupler_ends,
     cdef unsigned long long _seed = seed
     cdef vector[int] _onehotpar = onehotpar
 
+    print('_onehotpar', onehotpar)
+
     cdef void* _interrupt_function
     if interrupt_function is None:
         _interrupt_function = NULL
