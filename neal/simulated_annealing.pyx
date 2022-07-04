eb11416fb91ea3d54e3dd751acb53913b7604b13
kemalaziezrachmansyah <108319129+kemalaziezrachmansyah@users.noreply.github.com>
2022-07-04 16:25:49 +0900

    Update simulated_annealing.pyx

--- neal/simulated_annealing.pyx
+++ neal/simulated_annealing.pyx
@@ -115,6 +115,8 @@ def simulated_annealing(num_samples, h, coupler_starts, coupler_ends,
     # allocate ndarray for energies
     energies_numpy = np.empty(num_samples, dtype=np.float64)
     cdef double[:] energies = energies_numpy
+    
+    print('states_numpy: ', states_numpy)
 
     # explicitly convert all Python types to C while we have the GIL
     cdef char* _states = &states_numpy[0,0]
