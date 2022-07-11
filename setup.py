cbec0b5a4143c1040e10b26114513d9396722e6f
kemalaziezrachmansyah <108319129+kemalaziezrachmansyah@users.noreply.github.com>
2022-07-11 16:02:52 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -64,9 +64,11 @@ if USE_CYTHON:
 
 packages = ['neal']
 
-install_requires = ['dimod>=0.9.11,<1.13.0',
-                    'numpy>=1.19.1,<2.0.0',
-                    ]
+install_requires = []
+
+#install_requires = ['dimod>=0.9.11,<1.13.0',
+#                    'numpy>=1.19.1,<2.0.0',
+#                    ]
 
 setup_requires = ['numpy>=1.19.1,<2.0.0,!=1.21.0,!=1.21.1']
 
