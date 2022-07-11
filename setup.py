268931e037dca18140f99f392b8792beb534971f
kemalaziezrachmansyah <108319129+kemalaziezrachmansyah@users.noreply.github.com>
2022-07-11 16:23:42 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -64,15 +64,15 @@ if USE_CYTHON:
 
 packages = ['neal']
 
-install_requires = []
+#install_requires = []
 
-#install_requires = ['dimod>=0.9.11,<1.13.0',
-#                    'numpy>=1.19.1,<2.0.0',
-#                    ]
+install_requires = ['dimod>=0.9.11,<1.13.0',
+                    'numpy>=1.19.1,<2.0.0',
+                    ]
 
-setup_requires = []
+#setup_requires = []
 
-# setup_requires = ['numpy>=1.19.1,<2.0.0,!=1.21.0,!=1.21.1']
+setup_requires = ['numpy>=1.19.1,<2.0.0,!=1.21.0,!=1.21.1']
 
 classifiers = [
     'License :: OSI Approved :: Apache Software License',
